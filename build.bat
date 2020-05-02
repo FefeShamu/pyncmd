34ca9e53da24d476e48836b9356652c88d8aa6a5
greats3an <greats3an@gmail.com>
2020-05-02 20:12:55 +0800

    removed build script

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pyinstaller -F --add-data "%cd%\;." pyncmd.py
-del pyncmd.exe
-copy dist\pyncmd.exe pyncmd.exe
-REM Cleanups.
-rmdir dist /S /Q
-rmdir build /S /Q
-rmdir __pycache__ /S /Q
-del *.spec
