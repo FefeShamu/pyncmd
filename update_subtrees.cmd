fd6bcdf2c686f1d48c17c3eaa92e8646c5b35eb7
greats3an <greats3an@gmail.com>
2020-05-02 13:33:40 +0800

    Update subtrees

--- update_subtrees.cmd
+++ update_subtrees.cmd
@@ -1,2 +1,3 @@
 @echo off
 git subtree pull --prefix=pyncm https://github.com/greats3an/pyncm master --squash
+git subtree pull --prefix=pywebserver https://github.com/greats3an/pywebserver master --squash
