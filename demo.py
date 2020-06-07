b3a9f84863387e475adc6774c98fb439f2b4ad6e
greats3an <greats3an@gmail.com>
2020-06-07 17:23:05 +0800

    Squashed 'pywebserver/' changes from 8062161..97a664c
    97a664c updates
    64598ca updated demo
    bb9d78a updated demo
    
    git-subtree-dir: pywebserver
    git-subtree-split: 97a664c8ae8fd406769ed4ad37cdac9adb5a48d3

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 from pywebserver import PyWebServer
 from pywebserver.modules import HTTPModules,PathMakerModules,HTTPStatus
 from pywebserver.handler import RequestHandler
+from pywebserver.adapter.websocket import Websocket
 import coloredlogs,io
 coloredlogs.install(level=0)
 # For coloring logs
@@ -53,14 +54,24 @@ pre {
 
 @server.route(PathMakerModules.Absolute('/'))
 def index(request : RequestHandler):
+    HTTPModules.RestrictVerbs(request,['POST'])
     HTTPModules.Redirect(request,'files')
 
 @server.route(PathMakerModules.DirectoryPath('/files/'))
 def subfolder(request : RequestHandler):
     HTTPModules.IndexFolder(request,'./' + request.path[7:],GetStyleSheet())
+@server.route(PathMakerModules.Absolute('/ws'))
+def websocket(request : RequestHandler):
+    ws = Websocket(request)
+    ws.handshake()
+    def callback(msg):
+        print(msg)
+        ws.send(b'recevied:' + msg[-1])
+    ws.callback = callback
+    ws.serve()
 
 server.error_message_format = f'<style>{GetStyleSheet()}</style>' + server.error_message_format
 # Adds the style sheet to the error page
 
-print('servering now')
+print('Serving http://localhost:3331 & ws://localhost:3331/ws')
 server.serve_forever()
