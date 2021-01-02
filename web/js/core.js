041789981ff405c601a20fca1f083857f8bc8082
greats3an <greats3an@gmail.com>
2021-01-02 10:54:47 +0800

    now compile-able with babel

---
+++ web/js/core.js
@@ -0,0 +1,191 @@
+const id_regex = /\d{5,}/gm;
+var vue = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: () => ({
+        newURL: '',
+
+        currentAudio: null,
+        currentTrack: null,
+        playlist: [],
+
+        loading: false,
+        loadingRecessive: false,
+        loadInfo: 'Please stand by',
+
+        existTracks: [],
+        trackConflictDialog: false,
+
+        lastError: null,
+        error: false,
+
+        audioConfig: {
+            bitrate: 320000
+        },
+
+        currentTime: 0,
+        duration: 0,
+
+        player: document.getElementById('player'),
+
+        currentLyrics: [],
+        matchedIndex: 0,
+        parsedLyrics: null,
+
+        searchResults: null,
+        queryString: null,
+        debounce: 500
+    }),
+    watch: {            
+        currentLyrics: (new_, old_) => {
+            if (!new_) return
+            try {
+                vue.parsedLyrics = parseLryics(new_.lrc.lyric, new_.romalrc.lyric, new_.tlyric.lyric)
+                console.log('[lyrics] parsed', vue.parsedLyrics)
+            } catch {
+                vue.parsedLyrics = null
+                vue.matchedIndex = 0
+            }
+
+        },
+        queryString: (new_, old_) => {
+            if (vue.queryTimeout)
+                clearTimeout(vue.queryTimeout)
+            vue.queryTimeout = setTimeout(() => {
+                // querying the result                    
+                if (!vue.queryString) return
+                vue.loadingRecessive = true
+                console.log('[search] searching', vue.queryString)
+                fetch('pyncm/cloudsearch/GetSearchResult?' + new URLSearchParams(Object.assign({
+                    keyword: vue.queryString
+                }))).then(response => response.json()).then(
+                    data => {
+                        vue.loadingRecessive = false
+                        if (!data.result || !data.result.songs) return
+                        console.log('[search] results', data.result.songs)
+                        vue.searchResults = data.result.songs
+                    }).catch(err => {
+                    vue.loadingRecessive = false
+                })
+            }, vue.debounce)
+        }
+    },
+    methods: {
+        setPlay: (evt) => {
+            if (!evt) return
+            vue.currentTrack = evt
+            vue.loadInfo = 'Fetching track audio'
+            vue.loading = true
+            fetch('pyncm/track/GetTrackAudio?' + new URLSearchParams(Object.assign({
+                song_ids: evt.id
+            }, vue.audioConfig))).then(response => response.json()).then(data => {
+                var track = data.data[0]
+                console.log(`[track] audio fetched. bitrate is ${track.br}`, track)
+                vue.currentLyrics = null
+                vue.currentAudio = track
+                // setup the player
+                vue.player.src = track.url
+                vue.player.play()
+                vue.loading = false
+            }).catch(err => {
+                vue.loading = false
+                vue.lastError = err
+                vue.error = true
+            }).then(data => {
+                fetch('pyncm/track/GetTrackLyrics?' + new URLSearchParams(Object.assign({
+                    song_id: evt.id
+                }))).then(response => response.json()).then(
+                    data => {
+                        console.log(`[lyrics] lyrics fetched for ${evt.id}`, data)
+                        vue.currentLyrics = data
+                    }
+                )
+            })
+        },
+        seekTrack: (pos) => {
+            vue.player.currentTime = pos
+        },
+        opearteTrack: (dir) => {
+            var index = vue.playlist.indexOf(vue.currentTrack)
+            var operation = {
+                forward: () => index++,
+                pause: () => {
+                    if (vue.player.paused) vue.player.play()
+                    else vue.player.pause()
+                    return true
+                },
+                rewind: () => index--,
+            } [dir]()
+            if (operation) return
+            index = index % vue.playlist.length
+            vue.currentTrack = vue.playlist[index]
+            vue.setPlay(vue.currentTrack)
+        },
+        addTrack: (url) => {
+            console.log(url)
+            var route, params , m
+            var ids = []
+            while ((m = id_regex.exec(url)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === id_regex.lastIndex) id_regex.lastIndex++
+                m.forEach(function (match, groupIndex) {
+                    ids.push(match)
+                });
+            }
+            var match = ids
+            if (!match) {
+                vue.lastError = 'No applicable IDs found'
+                vue.error = true
+            }
+            match = match[0]
+            // parsing ids,only picking up first match
+            route = 'track/GetTrackDetail'
+            params = {
+                song_ids: match
+            }
+            if (check_arg(url, 'list')) {
+                route = 'playlist/GetPlaylistInfo'
+                params = {
+                    playlist_id: match
+                }
+            } else if (check_arg(url, 'album')) {
+                route = 'album/GetAlbumInfo'
+                params = {
+                    album_id: match
+                }
+            }
+            console.log(`[multi] adding ${match} ${route}`)
+            vue.loadInfo = 'Fetching tracks'
+            vue.loading = true;
+            fetch(`pyncm/${route}?` + new URLSearchParams(params)).then(response => response.json())
+                .then(data => {
+                    var newTracks, existTracks;
+                    if (data.playlist) data.songs = data.playlist.tracks
+                    newTracks = data.songs.filter(track => !vue.playlist.map(track => track.id)
+                        .includes(track.id))
+                    if (newTracks.length != data.songs.length)
+                        existTracks = data.songs.filter(track => vue.playlist.map(track => track.id)
+                            .includes(track.id))
+                    vue.existTracks = existTracks
+                    if (existTracks) vue.trackConflictDialog = true
+                    vue.playlist.push(...newTracks)
+                    if (!vue.currentTrack) vue.setPlay(vue.playlist[0])
+                    vue.loading = false
+                }).catch(error => {
+                    vue.lastError = error
+                    vue.loading = false
+                    vue.error = true
+                })
+        },
+        lyricsAt: index => vue.parsedLyrics[Object.keys(vue.parsedLyrics)[index]],
+    }
+})
+vue.player.onended = () => {
+    vue.opearteTrack('forward')
+}
+vue.player.ontimeupdate = () => {
+    vue.duration = vue.player.duration
+    vue.currentTime = vue.player.currentTime
+    if (vue.parsedLyrics) vue.matchedIndex = search(vue.currentTime, Object.keys(vue.parsedLyrics))
+}
+vue.player.crossOrigin = "anonymous"
---
+++ web/js/fftinit.js
@@ -0,0 +1,28 @@
+var ffta_settings = {
+    'barStyle': ['#e2f1f8', '#e2f1f8', '#e2f1f8', '#b0bec5', '#b0bec5', '#b0bec5', '#808e95', '#808e95'],
+    'spacing': 2,
+    'ratio': 0.7,
+    'fftSize': 512,
+    'threshold': 3,
+    'range_l': 0,
+    'range_r': 0.3,
+    'accel': 0.95,
+    'apush': 30,
+    'sequence': () => {
+        draw_bass_response()
+        draw_bars();
+    }
+}
+
+var peakmeter = document.getElementById('visualizer')
+var audioCtx = new window.AudioContext()
+// connecting the analyzer    
+var source = audioCtx.createMediaElementSource(vue.player)
+source.connect(audioCtx.destination)
+var analyzer = audioCtx.createAnalyser()
+source.connect(analyzer)
+vue.player.addEventListener('play', function () {
+    audioCtx.resume();
+});
+ffta_init(analyzer, peakmeter, peakmeter.offsetWidth, peakmeter.offsetHeight, ffta_settings)
+requestAnimationFrame(ffta_draw);
